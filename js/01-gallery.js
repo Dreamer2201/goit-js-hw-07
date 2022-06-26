8f3f5536d3c27fe4fe60c1ad38be302c75fa1ff4
Oksana Polyakova <ddreamer22011986@gmail.com>
2022-06-26 15:27:38 +0300

    point 2 Task#1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,10 +2,12 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const listGallery = document.querySelector('.gallery');
+
+
 listGallery.insertAdjacentHTML('beforeend', createGallleryMarkup(galleryItems));
 
-function createGallleryMarkup (galleryItems) {
-    return galleryItems.map(({preview, original, description}) => {
+function createGallleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
         return `<div class="gallery__item">
             <a class="gallery__link" href="${original}">
                 <img
@@ -16,5 +18,17 @@ function createGallleryMarkup (galleryItems) {
                 />
             </a>
         </div>`;
-        }).join('');
+    }).join('');
 };
+
+listGallery.addEventListener('click', onImgGalleryClick);
+
+function onImgGalleryClick(event) {
+    event.preventDefault();
+const imgElSelected = event.target.classList.contains('gallery__image');
+
+if (!imgElSelected) {
+    return;
+}
+console.log(event.target.getAttribute('data-source'));
+}
