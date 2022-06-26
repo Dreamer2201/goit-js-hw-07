e9780cbc78f9224a327fb7f19ed7697f2df5e83b
Oksana Polyakova <ddreamer22011986@gmail.com>
2022-06-26 19:53:01 +0300

    markup-task#2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const listGallery = document.querySelector('.gallery');
+
+
+listGallery.insertAdjacentHTML('beforeend', createGallleryMarkup(galleryItems));
+
+function createGallleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    }).join('');
+};
+
+listGallery.addEventListener('click', onImgGalleryClick);
+
+function onImgGalleryClick(event) {
+    event.preventDefault();
+const imgElSelected = event.target.classList.contains('gallery__image');
+
+if(!imgElSelected) {
+    return;
+} 
+    const parentLink = event.target.closest('.gallery__item');
+    console.log(parentLink);
+};
 
 console.log(galleryItems);
